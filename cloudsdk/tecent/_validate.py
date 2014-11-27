88d81ecf8baa3d34e2890313ae807c793cadd15b
Elian Wan <elians.wan@everhomes.com>
2014-11-27 10:40:05 +0800

    delete file
    delete file

---
+++ cloudsdk/tecent/_validate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+__author__ = 'wan'
+from cloudsdk.tool.logger import LogFactory
+from cloudsdk.rest.request import RequestError
+
+logger = LogFactory.logger(__name__)
+
+
+def validate_rsp(rsp, action):
+    if isinstance(rsp, dict):
+        logger.error("Response message:", rsp, "current action", action)
+        raise RequestError(rsp['status'], rsp['message'])
+    rsp = eval(rsp)
+    if not str(eval(rsp)['code']).__eq__('0'):
+        logger.error("Response message:", rsp, "current action", action)
+        raise RequestError(rsp['code'], rsp['message'])
---
+++ cloudsdk/tecent/datacenter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+__author__ = 'wan'
+from cloudsdk.api.datacenter import DCSupport
+from _validate import validate_rsp
+
+
+class TecentDCSupport(DCSupport):
+    def __init__(self, ctx):
+        setattr(ctx, 'host', 'trade.{0}'.format(ctx.host))
+        DCSupport.__init__(self, ctx)
+
+    def list_regions(self):
+        rsp = self.request.invoke(scheme='https', Action='DescribeProductRegionList', instanceType=1)
+        validate_rsp(rsp, 'DescribeProductRegionList')
+        rsp = eval(rsp)
+        return eval(rsp)['availableRegion'].keys()
+
+    def list_zones(self):
+        raise NotImplementedError("The qcloud does not support the method")
