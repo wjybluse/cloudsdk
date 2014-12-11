7a81476467a96fb1334cf36b0936962609358e06
Elian Wan <elians.wan@everhomes.com>
2014-12-11 16:56:28 +0800

    update scaling code

--- scaling/test/proxy.py
+++ scaling/test/proxy.py
@@ -1,1 +1,6 @@
+# -*- coding: utf-8 -*-
 __author__ = 'wan'
+from scaling._scaling.scale import ScaleProxy
+
+if __name__ == '__main__':
+    proxy = ScaleProxy(None)
--- scaling/test/sched.py
+++ scaling/test/sched.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wan'
-import uuid
+import unittest
 from scaling.rule.timerwheel import TimerWheel
 
 
-def ttt():
-    print(str(uuid.uuid1()))
-
-
-if __name__ == '__main__':
-    tw = TimerWheel()
-    tw.add_task('hahh', ttt)
-    tw.start()
+class TestWheel(unittest.TestCase):
+    def test_wheel_ok(self):
+        tw = TimerWheel()
+        tw.start()
